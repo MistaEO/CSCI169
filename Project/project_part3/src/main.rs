2021f344e7af96e45bc66c951169b324d1c99977
Gavin J <gavinyjiang@gmail.com>
2023-02-25 15:56:47 -0800

    Project Phase 3 Finished

--- Project/project_part3/src/main.rs
+++ Project/project_part3/src/main.rs
@@ -1,14 +1,45 @@
-use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt}};
+use tokio::{net::TcpListener, io::{AsyncWriteExt, BufReader, AsyncBufReadExt}, sync::broadcast};
 
 #[tokio::main]
 async fn main(){
     let listener = TcpListener::bind("localhost:8000").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (tx,mut _rx) = broadcast::channel(10);
 
-    let mut buffer = [0u8; 1024];
+    loop{
 
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move{
+            let (reader, mut writer) = socket.split();
     
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+            let mut reader = BufReader::new(reader);
+        
+            let mut line = String::new();
+            loop{
+                tokio::select!{
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0{
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr{
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+
+                        }
+                    }
+                }
+            }
+        });
+    }
+    
+    
 }
